d4ed468a6e53827bfc9f70b32708c8495b8cebfa
Ron Kurr <kurr@kurron.org>
2018-04-01 15:04:28 -0400

    Use the newer Spring Boot build model.

---
+++ src/main/java/org/kurron/Application.java
@@ -0,0 +1,15 @@
+package org.kurron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application driver.
+ */
+@SpringBootApplication
+class Application {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( Application.class, args );
+    }
+}
